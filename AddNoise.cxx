c0edf2a93d80ed7d83ce9d156bd42459c68e26d4
Cory Quammen <cquammen@cs.unc.edu>
2012-10-20 12:09:50 -0400

    Added reading/writing of image files

--- AddNoise.cxx
+++ AddNoise.cxx
@@ -2,6 +2,11 @@
 
 #include <cstdlib>
 
+// Core ITK classes
+#include <itkImageFileReader.h>
+#include <itkImageFileWriter.h>
+
+// Noise classes
 #include <itkAdditiveGaussianNoiseImageFilter.h>
 #include <itkShotNoiseImageFilter.h>
 
@@ -12,10 +17,10 @@ int main( int argc, char* argv[] )
 {
   // Parse arguments
   bool inputSet     = false;
-  std::string inputName;
+  std::string inputFile;
 
   bool outputSet    = false;
-  std::string outputName;
+  std::string outputFile;
  
   bool noiseSet = false;
   std::string noise;
@@ -29,14 +34,50 @@ int main( int argc, char* argv[] )
   bool scaleSet     = false;
   double scale      = 1.0;
 
-  if ( !GetArgument( argc, argv, "--input",  true,  inputName,  inputSet  ) ||
-       !GetArgument( argc, argv, "--output", true,  outputName, outputSet ) ||
+  if ( !GetArgument( argc, argv, "--input",  true,  inputFile,  inputSet  ) ||
+       !GetArgument( argc, argv, "--output", true,  outputFile, outputSet ) ||
        !GetArgument( argc, argv, "--noise",  true,  noise,      noiseSet  ) ||
        !GetArgument( argc, argv, "--mean",   false, mean,       meanSet   ) ||
        !GetArgument( argc, argv, "--stdev",  false, stdev,      stdevSet  ) ||
        !GetArgument( argc, argv, "--scale",  false, scale,      scaleSet  ) )
     {
     return EXIT_FAILURE;
+    }
+
+  // Check noise argument for validity
+  if ( noise != "Poisson" && noise != "Gaussian" )
+    {
+    std::cerr << "--noise argument must be 'Poisson' or 'Gaussian'."
+              << "It was '" << noise << "'." << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  // Set up useful typedefs
+  typedef double                              PixelType;
+  const unsigned int                          Dimension = 3;
+  typedef itk::Image< PixelType, Dimension >  ImageType;
+  typedef itk::ImageFileReader< ImageType >   ReaderType;
+  typedef itk::ImageFileWriter< ImageType >   WriterType;
+
+  // Read input image
+  try
+    {
+    ReaderType::Pointer reader = ReaderType::New();
+    reader->SetFileName( inputFile.c_str() );
+
+    // Add noise
+
+
+    // Write output image
+    WriterType::Pointer writer = WriterType::New();
+    writer->SetFileName( outputFile.c_str() );
+    writer->SetInput( reader->GetOutput() );
+    writer->Update();
+    }
+  catch ( itk::ExceptionObject & e )
+    {
+    std::cerr << "Exception caught" << std::endl;
+    std::cerr << e << std::endl;
     }
 
   return EXIT_SUCCESS;
